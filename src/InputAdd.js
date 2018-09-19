b619c21c9eb3faeb0a3887a86fb64f910dde7b68
marco <gundul1@libero.it>
2018-09-19 18:21:53 +0200

    Aggiunte liste selezioni streamer presenti e necessari

--- src/InputAdd.js
+++ src/InputAdd.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 class InputAdd extends Component {
 
+  changePresenti(e) {
+    console.log(e.target.value);
+    document.querySelector('.necessari').value=parseInt(e.target.value) + 1;
+    let presenti = document.querySelectorAll('.necessari > option');
+    presenti.forEach((element, index) => {
+      if(index < e.target.value - 1)
+      {
+        element.disabled = true;
+      } else {
+        element.disabled = false;
+      }
+    })
+  }
+
   render() {
     return (
       <div className="InputAdd">
@@ -9,6 +23,22 @@ class InputAdd extends Component {
         <input type="text" id="txtTitle" placeholder="inserire titolo"></input><br/>
         Num:
         <input type="text" id="txtNumber" placeholder="inserire numero partecipanti"></input><br/>
+        Pres:
+        <select className="presenti" onChange={this.changePresenti}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        Necess:
+        <select className="necessari">
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+        </select><br/>
         <button className="AddInfoButton" onClick={this.props.addToBD}>Aggiunti alla lista</button>
       </div>
     )
