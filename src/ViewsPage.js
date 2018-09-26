10eb5bc00a75dd6ef0ccee5b6a20d73b9c18d042
marco <gundul1@libero.it>
2018-09-26 08:58:12 +0200

    finito calcolo proporzioni delle view

--- src/ViewsPage.js
+++ src/ViewsPage.js
@@ -8,63 +8,37 @@ class ViewsPage extends Component {
     // Adatto le dimenzioni delle views e delle chat al loro numero
     let views = document.querySelectorAll(".SingleView");
     let chatButtons = document.querySelectorAll(".ChatButton");
-    /*switch(views.length) {
-      case 1:
-        document.querySelector(".ChatButtons").style.display = "none";
-        document.querySelector(".single_chat").style.height = "100%";
-        views.forEach((element) =>
-        {
-          element.style.height = "100%";
-          element.style.width = "100%";
-        });
-        break;
-      case 2:
-        chatButtons.forEach((element) =>
-        {
-          element.style.height = "100%";
-          element.style.width = "50%";
-        });
-        views.forEach((element) =>
-        {
-          element.style.height = "50%";
-          element.style.width = "100%";
-        });
-        break;
-      case 3:
-        chatButtons.forEach((element) => element.style.width = "50%");
-        views.forEach((element) => element.style.width = "100%");
-        break;
-      case 4:
-        chatButtons.forEach((element) => element.style.width = "50%");
-        views.forEach((element) => element.style.height = "50%");
-        break;
-    }*/
     let altezza = document.querySelector(".VideosContainer").offsetHeight;
     let larghezza = document.querySelector(".VideosContainer").offsetWidth;
+
     altezza = altezza / 9;
     larghezza = larghezza / 16;
-    /*if(altezza > larghezza)
-    {
-      let righe = (altezza/larghezza);
-      console.log("righe: ", righe);
-    } else {
-      let colonne = (larghezza/altezza);
-      console.log("colonne: ", colonne);
-    }*/
     let res = altezza / larghezza;
+    let calcolo = Math.ceil(views.length / res);
+    let robba = (Math.floor((res) * 2));
+
     if(res > 1)
     {
-      console.log("alto");
-    } else
-    {
-      console.log("largo");
-      let calcolo = (Math.floor(res * views.length) + 1);
-      console.log("calcolo", calcolo);
-      console.log("leng", views.length);
       views.forEach((element) =>
       {
-        let robba = (Math.ceil((1/res) * 2));
-        console.log("vr", robba);
+        if(views.length <= robba)
+        {
+          element.style.height = "calc( 100% / " + views.length +")";
+          element.style.width = "100%";
+        } else {
+          while((calcolo - (views.length-calcolo)) >= 2)
+          {
+            calcolo--;
+          }
+          let cos = Math.ceil((views.length / calcolo));
+          element.style.width = "calc( 100% / " + cos +")";
+          element.style.height = "calc( 100% / " + calcolo +")";
+        }
+      });
+    } else
+    {
+      views.forEach((element) =>
+      {
         if(views.length < robba)
         {
           element.style.width = "calc( 100% / " + views.length +")";
@@ -75,7 +49,6 @@ class ViewsPage extends Component {
             calcolo--;
           }
           let cos = Math.ceil((views.length / calcolo));
-          console.log("cos", cos);
           element.style.width = "calc( 100% / " + calcolo +")";
           element.style.height = "calc( 100% / " + cos +")";
         }
