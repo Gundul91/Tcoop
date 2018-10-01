ded27988811c95995ca424c88f8c6f86cb55895e
marco <gundul1@libero.it>
2018-10-01 10:04:08 +0200

    modificato dome di qualche classe

--- src/Chat.js
+++ src/Chat.js
@@ -4,7 +4,7 @@ class Chat extends Component {
   render() {
     return (
       <div id={"chat_embed_" + this.props.index} className={this.props.className}>
-        <p class="chat_title">{this.props.nick}</p>
+        <p class="chatTitle">{this.props.nick}</p>
         <iframe frameborder="0"
             scrolling="yes"
             src={"https://www.twitch.tv/embed/" + this.props.nick + "/chat"}
--- src/ViewsPage.js
+++ src/ViewsPage.js
@@ -8,8 +8,8 @@ class ViewsPage extends Component {
   viewsSize() {
     // Adatto le dimenzioni delle views e dei bottoni delle chat al loro numero
     let views = document.querySelectorAll(".SingleView");
-    let altezza = document.querySelector(".VideosContainer").offsetHeight;
-    let larghezza = document.querySelector(".VideosContainer").offsetWidth;
+    let altezza = document.querySelector(".videosContainer").offsetHeight;
+    let larghezza = document.querySelector(".videosContainer").offsetWidth;
 
     let best = {w: 0, h: 0};
     let w, h;
@@ -39,17 +39,17 @@ class ViewsPage extends Component {
     });
 
     // Calcolo dimensioni bottoni chat
-    if((views.length * 33) + 38 > document.querySelector(".ViewsPage").offsetHeight)
+    if((views.length * 33) + 38 > document.querySelector(".viewsPage").offsetHeight)
     {
-      let height = ((document.querySelector(".ViewsPage").offsetHeight - 38 - (3 * views.length)) / views.length);
-      document.querySelector(".ChatButtons").style.fontSize = (height < 15) ? "0.7em" : "1em";
-      document.querySelectorAll(".ChatButton").forEach((el) => {
+      let height = ((document.querySelector(".viewsPage").offsetHeight - 38 - (3 * views.length)) / views.length);
+      document.querySelector(".chatButtons").style.fontSize = (height < 15) ? "0.7em" : "1em";
+      document.querySelectorAll(".chatButton").forEach((el) => {
         el.style.height = height  + "px";
       });
-    } else if(document.querySelector(".ChatButton").style.height !== "30px")
+    } else if(document.querySelector(".chatButton").style.height !== "30px")
     {
-      document.querySelector(".ChatButtons").style.fontSize = "1em";
-      document.querySelectorAll(".ChatButton").forEach((el) => {
+      document.querySelector(".chatButtons").style.fontSize = "1em";
+      document.querySelectorAll(".chatButton").forEach((el) => {
         el.style.height = "30px";
       });
     }
@@ -57,9 +57,9 @@ class ViewsPage extends Component {
 
   // show the selected chat and hide the other
   chatClick(index, el) {
-    let chats = document.querySelectorAll(".single_chat");
-    document.querySelector(".selected_chat").classList.remove("selected_chat");
-    document.getElementById('chat_embed_' + index).classList.add("selected_chat");
+    let chats = document.querySelectorAll(".singleChat");
+    document.querySelector(".selectedChat").classList.remove("selectedChat");
+    document.getElementById('chat_embed_' + index).classList.add("selectedChat");
   }
 
   // Funzione eseguita al click del bottone di apertura/chiusura chat
@@ -67,14 +67,14 @@ class ViewsPage extends Component {
     if(el.target.classList.contains("closeChat"))
     {
       document.querySelector(".chatIcon").src="https://png.icons8.com/metro/1600/chat.png";
-      document.querySelector(".Chats").style.display = "none";
-      document.querySelector(".VideosContainer").style.width = "100%";
+      document.querySelector(".chats").style.display = "none";
+      document.querySelector(".videosContainer").style.width = "100%";
       window.dispatchEvent(new Event('resize')); // Scateno l'evento di "resize" della pagina per far riadattare le views
       el.target.classList.remove("closeChat");
     } else {
       document.querySelector(".chatIcon").src = "https://png.icons8.com/metro/1600/no-chat.png";
-      document.querySelector(".Chats").style.display = "inline-block";
-      document.querySelector(".VideosContainer").style.width = "calc(100% - 341px)";
+      document.querySelector(".chats").style.display = "inline-block";
+      document.querySelector(".videosContainer").style.width = "calc(100% - 341px)";
       window.dispatchEvent(new Event('resize'));
       el.target.classList.add("closeChat");
     }
@@ -82,7 +82,7 @@ class ViewsPage extends Component {
 
   componentDidMount () {
     if(document.querySelectorAll(".SingleView").length === 1)
-      document.querySelector(".ChatButtons").style.display = "none";
+      document.querySelector(".chatButtons").style.display = "none";
     this.viewsSize();
     window.addEventListener("resize", this.viewsSize.bind(this));
   }
@@ -96,8 +96,8 @@ class ViewsPage extends Component {
      * Aggiunto anche un bottone ed una chat per ogni streamer
      */
     return (
-      <div className="ViewsPage">
-        <div className="VideosContainer">
+      <div className="viewsPage">
+        <div className="videosContainer">
           {
             streamers.map((item, index) =>
               <SingleView
@@ -108,12 +108,12 @@ class ViewsPage extends Component {
           }
         </div>
         <button className="closeButton closeChat" onClick={this.closeChat}><img className="chatIcon" src="https://png.icons8.com/metro/1600/no-chat.png"></img></button>
-        <div className="Chats">
-          <div className="ChatButtons">
+        <div className="chats">
+          <div className="chatButtons">
             {
               streamers.map((item, index) =>
                 <button
-                  className="ChatButton"
+                  className="chatButton"
                   onClick={this.chatClick.bind(this, index)}
                 >{item}</button>
               )
@@ -124,7 +124,7 @@ class ViewsPage extends Component {
               <Chat
                 nick={item}
                 index={index}
-                className={(!index) ? "single_chat selected_chat" : "single_chat"}
+                className={(!index) ? "singleChat selectedChat" : "singleChat"}
               />
             )
           }
